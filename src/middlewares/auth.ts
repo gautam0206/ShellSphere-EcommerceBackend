8c3b244a3e60cf77b026e14893f1a77eaf697580
GAUTAM JAIN <g9213733800@gmail.com>
2024-03-13 22:44:41 +0530

    Initial commit

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+// Middleware to make sure only admin is allowed
+import { Request, Response, NextFunction } from "express";
+
+import { TryCatch } from "./error.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { User } from "../models/user.js";
+
+export const adminOnly = TryCatch(async (req, res, next) => {
+    const { id } = req.query;
+  
+    if (!id) return next(new ErrorHandler("Please Login First", 401));
+  
+    const user = await User.findById(id);
+    if (!user) return next(new ErrorHandler("Id Not exist", 401));
+    if (user.role !== "admin")
+      return next(new ErrorHandler("only admin can access this", 403));
+  
+    next();
+    // iske bad next wala func call hojayega route ka
+  });
